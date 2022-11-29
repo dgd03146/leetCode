bedeffb716aef2c46c66b23d5e43a87d96a6b5ca
Jeong <84106842+dgd03146@users.noreply.github.com>
2022-11-29 18:59:56 +0900

    Time: 99 ms, Memory: 42 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -8,8 +8,7 @@ var intersect = function(nums1, nums2) {
     
     for(let i of nums1){
       if(nums2.includes(i) ){
-           nums2[nums2.indexOf(i)] = -1
-          console.log(nums2)
+          nums2[nums2.indexOf(i)] = -1
           answer.push(i)
       }
   }
